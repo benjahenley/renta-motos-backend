fc6f7e1e88df03f22b78c0529e2d3c43bfbec0c8
DevWebPM <benjahenley@hotmail.com>
2024-06-20 17:04:21 -0300

    s;

--- src/app/api/jetskis/route.ts
+++ src/app/api/jetskis/route.ts
@@ -1,10 +1,11 @@
 import { getAllJetskis } from "@/controllers/jetskis";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    console.log("paso 1");
     const jetskis = await getAllJetskis();
+    const response = NextResponse.json(jetskis);
+    response.headers.set("Cache-Control", "s-maxage=1, stale-while-revalidate");
     return NextResponse.json(jetskis);
   } catch (e: unknown) {
     return NextResponse.json({ error: (e as Error).message }, { status: 500 });
